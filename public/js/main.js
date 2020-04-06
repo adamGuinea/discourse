999cc54af4c0ee99b955b1e89b9641b34d40c666
Adam Guinea <43794777+adamGuinea@users.noreply.github.com>
2020-04-06 20:30:59 +1000

    displaying new messages in client

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,34 @@
-const chatForm = document.getElementById('chat-form')
+const chatForm = document.getElementById("chat-form");
+const chatMessages = document.querySelector('.chat-messages');
 
+//message from server
 const socket = io();
-socket.on("message", (message) => console.log(message));
+socket.on("message", (message) => {
+  outputMessage(message);
+
+  //scroll down
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+});
 
 // on message submit
-chatForm.addEventListener('submit', e => {
-    e.preventDefault();
+chatForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    const msg = e.target.elements.msg.value;
+  const msg = e.target.elements.msg.value;
 
-    
-})
+  //emit it to the server
+  socket.emit("chatMessage", msg);
+
+  e.target.elements.msg.value = ''
+  e.target.elements.msg.focus();
+});
+
+function outputMessage(message) {
+  const div = document.createElement("div");
+  div.classList.add("message");
+  div.innerHTML = `<p class="meta">Mary <span>9:15pm</span></p>
+    <p class="text">
+      ${message}
+    </p>`;
+  document.querySelector(".chat-messages").appendChild(div);
+}
--- server/index.ts
+++ server/index.ts
@@ -19,8 +19,12 @@ io.on("connection", (socket: any) => {
   socket.broadcast.emit("message", "A new user has joined the chat");
 
   //when someone disconnects
-
   socket.on("disconnect", () => io.emit("message", "A user has left the chat"));
+
+  //listen for chat
+  socket.on("chatMessage", (msg: string) => {
+    io.emit("message", msg);
+  });
 });
 
 const PORT = process.env.PORT || 3000;
